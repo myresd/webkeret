fcd48e3c040c536a40c7b4c5419a605384b7d5e6
h864848 <h864848@stud.u-szeged.hu>
2020-05-10 22:53:25 +0200

    Elso commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'szeged-transport-map';
+
+  @ViewChild('mapContainer', {static: false}) gmap: ElementRef;
+map: google.maps.Map;
+lat = 46.247039;
+lng = 20.147005;
+coordinates = new google.maps.LatLng(this.lat, this.lng);
+mapOptions: google.maps.MapOptions = {
+  center: this.coordinates,
+  zoom: 15,
+};
+mapInitializer() {
+  this.map = new google.maps.Map(this.gmap.nativeElement, 
+  this.mapOptions);
+  this.marker.setMap(this.map);
+ }
+
+ ngAfterViewInit() {
+  this.mapInitializer();
+ }
+
+ marker = new google.maps.Marker({
+  position: this.coordinates,
+  map: this.map,
+});
+
 }
+
+
